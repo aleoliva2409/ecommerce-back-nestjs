ac4f06de329db44979d6e364ccdece8fed82433c
Alejandro Oliva <alejandro.oliva2409@gmail.com>
2023-09-25 23:20:44 -0300

    refactor: products and variants dtos created

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,41 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsNumber,
+  IsObject,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Min,
+  MinLength,
+} from 'class-validator';
+import { CreateVariantDto } from 'src/variants/dto/create-variant.dto';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(4)
+  title: string;
+
+  @IsString()
+  @MinLength(4)
+  description: string;
+
+  @IsString()
+  @IsOptional()
+  brand?: string = null;
+
+  @IsNumber()
+  @Min(0)
+  @IsPositive()
+  price: number;
+
+  @IsArray()
+  @IsOptional()
+  tags?: string[] = [];
+
+  @IsNumber()
+  @Min(0)
+  categoryId: number;
+
+  @IsObject()
+  variant: CreateVariantDto;
+}
--- src/variants/dto/create-variant.dto.ts
+++ src/variants/dto/create-variant.dto.ts
@@ -1,1 +1,19 @@
-export class CreateVariantDto {}
+import { IsNumber, IsPositive, IsString, Min } from 'class-validator';
+
+export class CreateVariantDto {
+  @IsString()
+  size: string; // enum
+
+  @IsNumber()
+  @Min(0)
+  @IsPositive()
+  stock: number;
+
+  @IsString()
+  color?: string = null;
+
+  // images: string[];
+
+  @IsNumber()
+  productId: number; // enum
+}
